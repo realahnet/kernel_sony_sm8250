fd95bdbc0e22aeeb2361bab70eda8d8664fe1238
Alexander Winkowski <dereference23@outlook.com>
2024-01-12 05:02:10 +0000

    Revert "ANDROID: mm: use raw seqcount variants in vm_write_*"
    This reverts commit 1091e4b2ffc0fd3971bd7594ef6decbaf67511d0.
    
    Change-Id: I5dc242f3d5aa05eb4630426d7444c36412ed6191
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1507,13 +1507,22 @@ static inline void vm_write_begin(struct vm_area_struct *vma)
          */
         if (!RB_EMPTY_NODE(&vma->vm_rb))
 		WARN_ON_ONCE(!rwsem_is_locked(&(vma->vm_mm)->mmap_sem));
-	/*
-	 * The reads never spins and preemption
-	 * disablement is not required.
-	 */
-	raw_write_seqcount_begin(&vma->vm_sequence);
+	write_seqcount_begin(&vma->vm_sequence);
+}
+static inline void vm_write_begin_nested(struct vm_area_struct *vma,
+					 int subclass)
+{
+	write_seqcount_begin_nested(&vma->vm_sequence, subclass);
 }
 static inline void vm_write_end(struct vm_area_struct *vma)
+{
+	write_seqcount_end(&vma->vm_sequence);
+}
+static inline void vm_raw_write_begin(struct vm_area_struct *vma)
+{
+	raw_write_seqcount_begin(&vma->vm_sequence);
+}
+static inline void vm_raw_write_end(struct vm_area_struct *vma)
 {
 	raw_write_seqcount_end(&vma->vm_sequence);
 }
@@ -1521,7 +1530,17 @@ static inline void vm_write_end(struct vm_area_struct *vma)
 static inline void vm_write_begin(struct vm_area_struct *vma)
 {
 }
+static inline void vm_write_begin_nested(struct vm_area_struct *vma,
+					 int subclass)
+{
+}
 static inline void vm_write_end(struct vm_area_struct *vma)
+{
+}
+static inline void vm_raw_write_begin(struct vm_area_struct *vma)
+{
+}
+static inline void vm_raw_write_end(struct vm_area_struct *vma)
 {
 }
 #endif /* CONFIG_SPECULATIVE_PAGE_FAULT */
