2f98751b712c1659b802ff80d24d8513705f0371
EmanuelCN <emanuelghub@gmail.com>
2024-08-07 13:57:13 +0300

    cpufreq: schedutil: Store the cached ratelimits values
    In a merge resolution google accidentally removed this, since we have checkouted the scheduler to redbull this part was missing.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -975,9 +975,11 @@ static void sugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
-	if (!count)
-		sugov_clear_global_tunables();
 
+	if (!count) {
+		sugov_tunables_save(policy, tunables);
+		sugov_clear_global_tunables();
+	}
 	mutex_unlock(&global_tunables_lock);
 
 	sugov_kthread_stop(sg_policy);
